7bd9c3195e17a39a6df8b557a8c75a51b2f160bb
mina-william-23 <capeandroid50@gmail.com>
2023-03-16 08:07:28 -0700

    add 101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,12 +3,12 @@
 /**
  * main - entry point of the programe
  *
- * Description: print string to starndard error using fwrite function
+ * Description: print string to standard error using fwrite function
  * Return: (1)
  */
 int main(void)
 {
-	char phrase[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char phrase[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	fwrite(phrase, sizeof(phrase), sizeof(char), Stderr);
 	return (1);
