fedf63d529383da46c28d417e21b9cab6c5dd12b
mina-william-23 <capeandroid50@gmail.com>
2023-03-17 07:34:47 -0700

    add 2-print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Description: print lowercase alphabet a-z
+ * using putchar function
+ * Return: (0) success
+*/
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		if (letter == 'z')
+			putchar('\n');
+		letter++;
+	}
+}
