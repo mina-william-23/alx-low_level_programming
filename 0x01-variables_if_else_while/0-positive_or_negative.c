b298a835eb985bb36050a41b2c82215f731c2d86
mina-william-23 <capeandroid50@gmail.com>
2023-03-17 06:51:30 -0700

    add 0-positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,6 +15,11 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
+	if (n == 0)
+		printf("%d is zero\n");
+	else if (n > 0)
+		printf("%d is positive\n");
+	else
+		printf("%d is negative\n");
 	return (0);
 }
