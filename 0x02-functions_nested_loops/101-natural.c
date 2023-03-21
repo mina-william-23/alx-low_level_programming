2e8afea12b5a020e53511933cea569bdbe50a346
mina-william-23 <capeandroid50@gmail.com>
2023-03-21 08:57:42 -0700

    add 101 natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Description: print sum of multiple of 3,5
+ * Return: (0) success
+ */
+
+int main(void)
+{
+	int sum = 0, num = 0;
+
+	while (num < 1024)
+	{
+		if (num % 3 == 0 || num % 5 == 0)
+			sum += num;
+		num++;
+	}
+	printf("%d\n", sum);
+	Return(0);
+}
