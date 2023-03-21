8ebec94a882d8c105a0e26e134351f63d190f148
mina-william-23 <capeandroid50@gmail.com>
2023-03-21 05:50:44 -0700

    ad 7 print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+include "main.h"
+
+/**
+ * print_last_digit - print last digit of argument
+ * @n: integer used to extract the last digit of it
+ *
+ * Return: return the printed last digit of (n)
+ */
+
+int print_last_digit(int n)
+{
+	_putchar('0' + (n % 10));
+	return (n % 10);
+}
