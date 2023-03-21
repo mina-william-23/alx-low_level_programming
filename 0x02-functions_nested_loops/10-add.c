0b4a693da21334a4f81cc2ebb7728579fca3d043
mina-william-23 <capeandroid50@gmail.com>
2023-03-21 07:13:14 -0700

    add 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - add two integers and return sum
+ * @n1: number1
+ * @n2: number2
+ *
+ * Return: sum of (n1) + (n2)
+ */
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
