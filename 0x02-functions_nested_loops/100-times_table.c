64c8e5a24075def0239182be7d7ec00525ea76f5
mina-william-23 <capeandroid50@gmail.com>
2023-03-21 08:26:34 -0700

    add 100 times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * print_times_table - print 0-15 times table
+ * @n: times to print
+ * Return: (0) success
+ */
+
+void print_times_table(int n)
+{
+	if (n < 0 || n > 15)
+		return (void);
+
+	int i, j, num;
+
+	for (i = 0; i < = n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			num = i * j;
+			if (j > 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				if (num >= 100)
+				{
+					_putchar('0' + (num / 100));
+					_putchar('0' + (num % 100 / 10));
+					_putchar('0' + (num % 1000));
+				}
+				else if (num > 9 && < 100)
+				{
+					_putchar('0' + ' ');
+					_putchar('0' + (num / 10));
+					_putchar('0' + (num % 10));
+				}
+				else
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar('0' + num);
+				}
+			}
+			else
+				_putchar('0' + num);
+		}
+		_putchar('\n');
+	}
+}
