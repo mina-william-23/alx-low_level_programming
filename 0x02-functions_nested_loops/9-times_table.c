cb358b95cc43d9972e3c4bf2a3bdbffc641f9055
mina-william-23 <capeandroid50@gmail.com>
2023-03-21 06:50:31 -0700

    add times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * times_table - print 0-9 times table
+ *
+ * Return: (0) success
+ */
+
+void times_table(void)
+{
+	int i, j, num;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			num = i * j;
+			if (num > 9)
+			{
+				_putchar('0' + (num / 10));
+				_putchar('0' + (num % 10));
+			}
+			else
+			{
+				_putchar('0' + num);
+			}
+
+			if (j < 0)
+			{
+				_putchar(',');
+				_putchar(9);
+			}
+
+		}
+		_putchar('\n');
+	}
+}
