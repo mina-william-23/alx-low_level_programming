56aec406ca2663a66650569ccddbb736f21eb4f2
mina-william-23 <125445282+mina-william-23@users.noreply.github.com>
2023-03-22 21:57:10 +0200

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -10,19 +10,19 @@
 int main(void)
 {
 	unsigned long int t = 1, j = 1, t1 = 0, t2 = 1, j1 = 0, j2 = 1, i;
-	
-	for(i = 0; i < 90; i++)
+
+	for (i = 0; i < 90; i++)
 	{
 		printf("%lu, ", t);
 		t = t + j;
 		j = t - j;
 	}
-	
+
 	t1 = t / 1000000000;
 	j1 = j / 1000000000;
 	t2 = t % 1000000000;
 	j2 = j % 1000000000;
-	
+
 	for (i = 90; i < 98; i++)
 	{
 		printf("%lu%lu", (t1 + (t2 / 1000000000)), (t2 % 1000000000));
