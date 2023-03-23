1707fc0770319ff3718127803c97be19a7aa4f51
mina-william-23 <capeandroid50@gmail.com>
2023-03-23 07:46:18 -0700

    add 101 print number

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_number - print integer using _putchar
+ * @n: inserted integer
+ * Description: reverse the number first
+ * then print it using _putchar()
+ */
+
+void print_number(int n)
+{
+	int old = n, rev = 0;
+
+	if (old < 0)
+		old *= -1;
+
+	while (old)
+	{
+		rev *= 10;
+		rev += old % 10;
+		old  /= 10;
+	}
+	if (n < 0)
+		_putchar('-');
+	while (rev)
+	{
+		_putchar('0' + (rev % 10));
+		rev /= 10;
+	}
+	_putchar('\n');
+
+}
