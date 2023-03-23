1a08e0f66605dbed2ee55679e71586cd1835173f
mina-william-23 <capeandroid50@gmail.com>
2023-03-23 06:03:07 -0700

    add 7 print diagnal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print diagnal by '\'
+ * @n: numbers of lines
+ * Description: everyline should have spaces
+ * equal to line numbers before '\' printed
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+			_putchar(' ');
+
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
