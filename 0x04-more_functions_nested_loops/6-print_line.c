50259630a9a241e83fc0e6f27fd358fd24ec132a
mina-william-23 <capeandroid50@gmail.com>
2023-03-23 05:42:06 -0700

    add 6-print-line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+include "main.h"
+
+/**
+ * print_line - print line with _putchar
+ */
+
+void print_line(int n)
+{
+	int cnt;
+
+	for (cnt = 0; cnt < n; cnt++)
+		_putchar('_');
+	_putchar('\n');
+}
