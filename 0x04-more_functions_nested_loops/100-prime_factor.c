81c25d54a26448f287d85e7525fc020e147c720a
mina-william-23 <capeandroid50@gmail.com>
2023-03-24 17:49:51 -0700

    add

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -6,17 +6,17 @@
 * Return: square root of n
 */
 
-double _sqroot(double n) 
+double _sqroot(double n)
 {
-  double i = 0;
-  double j = n / 2;
-  
-  while (j != i)
-  {
-    i = j;
-    j = (n / i + i) / 2;
-  }
-  return j;
+	double i = 0;
+	double j = n / 2;
+
+	while (j != i)
+	{
+		i = j;
+		j = (n / i + i) / 2);
+	}
+	return (j);
 }
 
 /**
@@ -26,37 +26,37 @@ double _sqroot(double n)
 
 void largest_prime_factor(long int n)
 {
-  int largest_prime, prime_num;
-  while (n % 2 == 0)
-  {
-    cout << 2 << " ";
-    n = n / 2;
-  }
-  for (prime_num = 3; prime_num <= _sqroot(n); prime_num = prime_num + 2)
-  {
-    while (n % prime_num == 0)
-    {
-      largest_prime = prime_num;
-      n = n / prime_num;
-    }
-  }
-  if (n > 2)
-    largest_prime = n;;
-  
-  printf("%d\n", largest_prime);
+	int largest_prime, prime_num;
+
+	while (n % 2 == 0)
+	{
+		cout << 2 << " ";
+		n = n / 2;
+	}
+	for (prime_num = 3; prime_num <= _sqroot(n); prime_num = prime_num + 2)
+	{
+		while (n % prime_num == 0)
+		{
+			largest_prime = prime_num;
+			n = n / prime_num;
+		}
+	}
+	if (n > 2)
+		largest_prime = n;
+
+	printf("%d\n", largest_prime);
 }
 
 /**
 * main - main
-
+*
 * Return: (0) success
 */
 
 int main(void)
 {
-  largest_prime_factor(612852475143);
-  
-  return (0);
+	largest_prime_factor(612852475143);
+	return (0);
 }
 
 
