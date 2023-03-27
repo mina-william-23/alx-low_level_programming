eea64e521111d6a71ec68c515d56ce29795fc466
mina-william-23 <capeandroid50@gmail.com>
2023-03-27 05:24:50 -0700

    add -puts half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _strlen - compute string length
+ *
+ * @s: pointer to string
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int length;
+
+	length = 0;
+	while (*s != '\0')
+	{
+		length++;
+		s = s + 1;
+	}
+	return (length);
+}
+
+/**
+ * puts_half - print second half of string
+ *
+ * @s: pointer to string
+ */
+
+void puts_half(char *s)
+{
+	int length, half_length;
+
+	length = _strlen(s);
+	half_length = length / 2;
+	while (half_length < length)
+		_putchar(s[half_length++]);
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -9,4 +9,5 @@ void print_rev(char *s);
 void rev_string(char *s);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+void puts_half(char *str);
 #endif
