866ad118a57b3e64abac3f39dd83e6367effd533
mina-william-23 <capeandroid50@gmail.com>
2023-03-26 22:08:19 -0700

    edit 4-print-rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,6 +1,26 @@
 #include "main.h"
 
 /**
+ * _strlen - compute string length
+ *
+ * @s: pointer to string
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int length;
+
+	length = 0;
+	while (*s != '\0')
+	{
+		length++;
+		s = s + 1;
+	}
+	return (length);
+}
+
+/**
  * print_rev - print strig in reverse mode
  *
  * @s: pointer to string
