74732d32751f6a675b5de750c35b9a2e0986de70
mina-william-23 <capeandroid50@gmail.com>
2023-03-29 11:20:39 -0700

    add 2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _strncpy - copy n characters from src to dest
+ *
+ * @dest: destination string pionter
+ * @src: source string pointer
+ * @n: number of characters to copy
+ * Return: dest string pointer
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *p;
+	int count;
+
+	p = dest;
+	count = 0;
+
+	while (count < n)
+	{
+		*dest = *src;
+
+		if (*src)
+			src++;
+		else
+			break;
+
+		dest++;
+		count++;
+	}
+	return (p);
+}
