e42faa43ef579532301121480ae5c6249ac18028
mina-william-23 <capeandroid50@gmail.com>
2023-03-29 13:37:41 -0700

    edit 6-cap-string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,24 +8,24 @@
 char *cap_string(char *str)
 {
 	char *current = str;
+	char before = ' ';
 	char sep[] = " \t\n,.;?!\"(){}";
 	int i, flag = 1;
 
 	while (*current)
 	{
+		flag = 0;
 		for (i = 0; i < 13; i++)
 		{
-			if (sep[i] == *current)
+			if (sep[i] == before)
 			{
 				flag = 1;
 				break;
 			}
 		}
 		if (*current >= 'a' && *current <= 'z' && flag)
-		{
 			*current -= 32;
-			flag = 0;
-		}
+		before = *current;
 		current++;
 	}
 	return (str);
