80441baed64f790805885b27748e5ebb81ecd0c3
mina-william-23 <capeandroid50@gmail.com>
2023-03-29 10:15:07 -0700

    add 7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * leet - encode to 1337
+ *
+ * @str: input string pointer
+ * Return: encoded string pointer
+*/
+char *leet(char *str)
+{
+	char *s;
+	int i;
+
+	char *letters[] = { 'a', 'e', 'o', 't', 'l' };
+	char *encode[] = { '4', '3', '0', '7', '1' };
+
+	s = str;
+
+	while (*s)
+	{
+		for (i = 0; i < 5; i++)
+			if (letters[i] == *s || letters[i] == *s - 'A' + 'a')
+				*s = encode[i];
+		s++;
+	}
+
+	return (str);
+}
