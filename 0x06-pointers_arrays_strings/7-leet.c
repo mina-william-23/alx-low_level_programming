90c6dd644137db714c889c35fee071c3966e0c03
mina-william-23 <capeandroid50@gmail.com>
2023-03-29 12:54:03 -0700

    edit 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,18 +1,16 @@
 #include "main.h"
 /**
  * leet - encode to 1337
- *
  * @str: input string pointer
  * Return: encoded string pointer
 */
 char *leet(char *str)
 {
-	char *s;
-	int i;
+	char *s = str;
+	unsigned int i;
 	char *letters[] = { 'a', 'e', 'o', 't', 'l' };
 	char *encode[] = { '4', '3', '0', '7', '1' };
 
-	s = str;
 
 	while (*s)
 	{
