cf87237dd476744bfd6a6f1c7550e16a8e293777
mina-william-23 <capeandroid50@gmail.com>
2023-03-29 08:11:46 -0700

    add 4 rev-array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse the array
+ *
+ * @a: pointer to array
+ * @n: size of array
+ */
+
+void reverse_array(int *a, int n)
+{
+	int half, temp, i;
+
+	half  = n / 2;
+	for (i = 0; i < n; i++)
+	{
+		temp = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = temp;
+	}
+}
