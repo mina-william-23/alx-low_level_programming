8ffc5019fe99b9e5ec4226b370205275fc1f00b3
mina-william-23 <capeandroid50@gmail.com>
2023-04-05 00:30:36 -0700

    add 5-sqrt-recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,15 +2,16 @@
 /**
  * _sqrt_recursion2 - calculate squreroot of number
  * @n: input
+ * @x: square root start
  * Return: squre root of n
 */
 int _sqrt_recursion2(int n, int x)
 {
 	if (x < 0)
-		return -1;
+		return (-1);
 	if (x * x == n)
-		return x;
-	_sqrt_recursion2(n, x-1);
+		return (x);
+	_sqrt_recursion2(n, x - 1);
 }
 /**
  * _sqrt_recursion - calculate squreroot of number
@@ -23,5 +24,5 @@ int _sqrt_recursion(int n)
 		return (-1);
 	if (n == 0 || n == 1)
 		return (n);
-	return (_sqrt_recursion2(n, n/2));
+	return (_sqrt_recursion2(n, n / 2));
 }
