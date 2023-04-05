6588f9671b522c82cc6f04d93797b5f89698d9b3
mina-william-23 <capeandroid50@gmail.com>
2023-04-04 20:29:57 -0700

    add 0-puts-recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _puts_recursion - print string s using recursion like puts function
+ * @s: string pointer
+*/
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(++s);
+	}
+}
