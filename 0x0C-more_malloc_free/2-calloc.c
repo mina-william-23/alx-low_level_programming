fbc1215bd41ebffd8474d596b68cca0e5d04d005
mina-william-23 <capeandroid50@gmail.com>
2023-04-12 22:11:31 -0700

    add 2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _calloc - create calloc using malloc
+ * @nmemb: number of elements
+ * @size: size of every element
+ * Return: void pointer to allocated memory or null if failed
+*/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *p = malloc(nmemb * size);
+	unsigned int i = 0;
+	unsigned char *cc;
+
+	if (nmemb == 0 || size == 0 || !p)
+		return (0);
+	/* cast void pointer to unsigned char as memset do to write in it */
+	cc = p;
+	for (i = 0; i < nmemb * size; i++)
+		*(cc + i) = 0;
+	return (p);
+}
