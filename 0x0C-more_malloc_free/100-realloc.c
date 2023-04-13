d6841328eeab32f8e4811e271458b2d6c5ab38f4
mina-william-23 <capeandroid50@gmail.com>
2023-04-13 11:48:08 -0700

    add 100-realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -9,36 +9,30 @@
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	char *p, *old;
-	unsigned int cnt;
 
-	if (!ptr)
-	{
-		if (new_size)
-			p = malloc(new_size);
-		else
-			p = 0;
-	}
-	else if (new_size == 0)
+	if (old_size == new_size)
+		return (ptr);
+
+	if (ptr == 0)
+		return (malloc(new_size));
+
+	if (new_size == 0)
 	{
 		free(ptr);
-		p = 0;
+		return (0);
 	}
-	else if (new_size == old_size)
-		p = ptr;
-	else
-	{
-		p = malloc(new_size);
-		if (!p)
-			return (0);
 
-		old = ptr;
-		cnt = old_size;
-		if (old_size > new_size)
-			cnt = new_size;
+	p = malloc(new_size);
+	old = ptr;
+	if (p == 0)
+		return (0);
 
-		while (cnt--)
-			p[cnt] = old[cnt];
-		free(old);
-	}
+	if (old_size > new_size)
+		old_size = new_size;
+
+	while (old_size--)
+		p[old_size] = old[old_size];
+
+	free(old);
 	return (p);
 }
