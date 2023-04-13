11071a231b465e2db611d664254cf5408806d7a0
mina-william-23 <capeandroid50@gmail.com>
2023-04-13 08:20:24 -0700

    edit 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,12 +10,12 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	unsigned int i = 0, len = nmemb * size;
-	char *p = malloc(len);
+	char *p = malloc(sizeof(char) * nmemb * size);
 
 	if (nmemb == 0 || size == 0 || p == 0)
 		return (0);
 
-	for (i = 0; i < len; i++)
+	for (i = 0; i < sizeof(char) * nmemb * size; i++)
 		*(p + i) = 0;
 	return (p);
 }
