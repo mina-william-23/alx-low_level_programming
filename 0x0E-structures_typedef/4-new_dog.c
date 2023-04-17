cfb88f2e2238941c8d0d49959c05c2639be14df1
mina-william-23 <capeandroid50@gmail.com>
2023-04-17 02:59:40 -0700

    edit 4-new-dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -43,10 +43,17 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	dog->name = _strcpy(name);
 	if (dog->name == NULL)
-		free(dog), return (NULL);
+	{
+		free(dog);
+		return (NULL);
+	}
 	dog->owner = _strcpy(owner);
 	if (dog->owner == NULL)
-		free(name), free(dog), return (NULL);
+	{
+		free(name);
+		free(dog);
+		return (NULL);
+	}
 	dog->age = age;
 
 	return (dog);
