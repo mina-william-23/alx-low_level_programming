dfc54a2265928edd48af79b72daca15c56264232
mina-william-23 <capeandroid50@gmail.com>
2023-04-18 22:56:04 -0700

    add 1-array-iterator

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+/**
+ * array_iterator- perform function action to all array members
+ * @array: array of ints
+ * @size: size of array
+ * @action: function pointer
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
