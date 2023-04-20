9ecfe3eef9ec16cd15b01f6947a9be91c63c7174
mina-william-23 <capeandroid50@gmail.com>
2023-04-20 02:54:19 -0700

    batch upload

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -10,13 +10,13 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list args;
 
-	if (n > 0 && separator)
+	if (n > 0)
 	{
 		va_start(args, n);
 		for (i = 0; i < n; i++)
 		{
 			printf("%d", va_arg(args, int));
-			if (i != n - 1)
+			if (i != n - 1 && separator)
 				printf("%s", separator);
 		}
 		va_end(args);
