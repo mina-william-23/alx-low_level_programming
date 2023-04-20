59d8d339020cd1bd3c49857fd9954399ce43fccf
mina-william-23 <capeandroid50@gmail.com>
2023-04-20 03:00:57 -0700

    batch upload

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -22,7 +22,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 				s = "(nil)";
 
 			printf("%s", s);
-	
+
 			if (i != n - 1 && separator)
 				printf("%s", separator);
 		}
