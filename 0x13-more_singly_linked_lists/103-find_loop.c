b1f35be86394cb8db06caf3410da9f30d255d46d
mina-william-23 <capeandroid50@gmail.com>
2023-04-30 16:20:54 -0700

    add 102

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,13 +1,13 @@
 #include "lists.h"
 /**
  * find_listint_loop - find loop in linkedlist  if exists
- * @h: head
+ * @head: head
  * Return: address of loop start of NULL if no loop
  */
 listint_t *find_listint_loop(listint_t *head)
 {
 	listint_t *slow, *fast, *loopstart;
-	
+
 	slow = fast = head;
 	/* detect if there is a loop */
 	while (fast && fast->next)
