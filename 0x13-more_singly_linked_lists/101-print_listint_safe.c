182d9090dbc4a2bb030289943009ffe78af9f87a
mina-william-23 <capeandroid50@gmail.com>
2023-04-30 04:43:33 -0700

    add 101-printsafe.c

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,46 @@
+#include "lists.h"
+/**
+ * print_listint_safe - print linked list (has loop or not)
+ * print the loop start if present
+ * @head: head
+ * Return: (size of linked list)
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t sz = 0;
+	const listint_t *slow = head, *fast = head, *loopStart = head;
+	int loop = 0;
+
+	while (fast && fast->next)
+	{
+		printf("[%p] %d\n", (void *)slow, slow->n);
+		slow = slow->next;
+		fast = fast->next->next;
+		sz++;
+		if (slow == fast)
+		{
+			loop = 1;
+			break;
+		}
+	}
+
+	while (slow)
+	{
+		if (loop)
+		{
+			if (slow == loopStart)
+			{
+				printf("-> [%p] %d\n", (void *)slow, slow->n);
+				break;
+			}
+			loopStart = loopStart->next;
+		}
+
+		printf("[%p] %d\n", (void *)slow, slow->n);
+		slow = slow->next;
+		sz++;
+	}
+
+	return (sz);
+}
+
