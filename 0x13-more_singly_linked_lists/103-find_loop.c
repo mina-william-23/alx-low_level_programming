0249cbb015ea2a19b440d55a8a8e5e4e9160cdbd
mina-william-23 <capeandroid50@gmail.com>
2023-04-30 16:49:33 -0700

    add 102

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -8,7 +8,7 @@ listint_t *find_listint_loop(listint_t *head)
 {
 	listint_t *slow, *fast;
 
-	if (!head || !head->next)
+	if (!head)
 		return (NULL);
 	slow = head;
 	fast = head;
@@ -31,7 +31,9 @@ listint_t *find_listint_loop(listint_t *head)
 			slow = slow->next;
 			fast = fast->next;
 		}
-		return (slow->next);
+		/* if slow == fast that means that both standing at loop start*/
+		/* if not that mean that next one is that loop start */
+		return (slow == fast ? slow ? slow->next);
 	}
 	return (NULL);
 }
