6c14d28c21ab47d3a9585912291aa85fdfef98f9
mina-william-23 <capeandroid50@gmail.com>
2023-06-15 04:35:19 -0700

    add task 0

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - print double linked list
+ * @h: head of list
+ * Return: size of linked list
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t sz = 0;
+	dlistint_t *head;
+
+	while (head)
+	{
+		head = head->next;
+		sz++;
+	}
+	return (sz);
+}
