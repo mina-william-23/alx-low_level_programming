b8ae966d1d7a1f887de9ee2db8815f8fdd85af12
mina-william-23 <capeandroid50@gmail.com>
2023-06-15 05:16:49 -0700

    add task 2

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - add node at begin
+ * @head: head of list
+ * n: value in new head
+ * Return: new head
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node = malloc(sizeof(dlistint_t *));
+
+	if (!new_node)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = NULL;
+
+	if (head && *head)
+	{
+		new_node->next = *head;
+		(*head)->prev = new_node;
+	}
+	*head = new_node;
+	return (new_node);
+}
