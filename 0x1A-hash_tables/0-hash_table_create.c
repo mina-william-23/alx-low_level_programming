ba917eabe3a83c38ac8069591b51bdd2c4682ccf
mina-william-23 <capeandroid50@gmail.com>
2023-07-06 11:18:36 -0700

    add task 2

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,6 +8,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hs_table = malloc(sizeof(hash_table_t));
+	unsigned long int i;
 
 	if (!hs_table)
 		return (NULL);
@@ -15,5 +16,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hs_table->array = malloc(sizeof(hash_node_t *) * size);
 	if (hs_table->array == NULL)
 		return (NULL);
+	for (i = 0; i < size; i++)
+		hs_table->array[i] = NULL;
 	return (hs_table);
 }
