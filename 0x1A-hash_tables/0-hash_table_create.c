ac8747db80d428a68e63293418a0c124184d3d33
mina-william-23 <capeandroid50@gmail.com>
2023-07-06 20:18:00 -0700

    fix 0 task

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,11 +7,16 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *hs_table = malloc(sizeof(hash_table_t));
+	hash_table_t *hs_table;
 	unsigned long int i;
 
+	if (size == 0)
+		return (NULL);
+
+	hs_table = malloc(sizeof(hash_table_t));
 	if (!hs_table)
 		return (NULL);
+
 	hs_table->size = size;
 	hs_table->array = malloc(sizeof(hash_node_t *) * size);
 	if (hs_table->array == NULL)
