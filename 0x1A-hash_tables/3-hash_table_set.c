d8b0b597691c643e9e8bb671c91e6b41007b983a
mina-william-23 <capeandroid50@gmail.com>
2023-07-06 18:01:38 -0700

    task 3 trial

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,12 +1,12 @@
 #include "hash_tables.h"
 /**
- * handle_collision - test if collision in hash table
+ * handle_dup_key - test if collision in hash table
  * @head: head of index of collision
  * @key: key to search for
  * @value: value fo key
  * Return: (1) collision resolved (0) no collision
  */
-int handle_collision(hash_node_t *head, const char *key, const char *value)
+int handle_dup_key(hash_node_t *head, const char *key, const char *value)
 {
 	hash_node_t *tmp = head;
 
@@ -36,29 +36,19 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (!ht || !key)
 		return (0);
+	
+	idx = key_index((const unsigned char *)key, ht->size);
+	if (handle_dup_key(ht->array[idx], key, value) == 1)
+		return (1);
 
 	new_node = malloc(sizeof(hash_node_t *));
 	if (!new_node)
 		return (0);
+
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
-	new_node->next = NULL;
+	new_node->next = ht->array[idx];
+	ht->array[idx] = new_node;
 
-	idx = key_index((const unsigned char *)key, ht->size);
-	if (ht->array[idx] == NULL)
-		ht->array[idx] = new_node;
-	else
-	{
-		if (handle_collision(ht->array[idx], key, value) == 1)
-		{
-			free(new_node->key);
-			free(new_node->value);
-			free(new_node);
-			return (1);
-		}
-		new_node->next = ht->array[idx];
-		ht->array[idx] = new_node;
-	}
 	return (1);
-
 }
