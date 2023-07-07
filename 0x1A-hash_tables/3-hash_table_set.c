fcb65f3aacb41d3e1b2f34e44b5cc4de40bd4bb0
mina-william-23 <capeandroid50@gmail.com>
2023-07-06 18:02:30 -0700

    task 3 trial

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -36,7 +36,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (!ht || !key)
 		return (0);
-	
+
 	idx = key_index((const unsigned char *)key, ht->size);
 	if (handle_dup_key(ht->array[idx], key, value) == 1)
 		return (1);
