c51cb1b087e2ad26678a4af5ea7afeb62bbc7621
mina-william-23 <capeandroid50@gmail.com>
2023-08-23 10:01:38 -0700

    test 5

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,19 @@
+#/usr/bin/python3
+''' island_perimeter module '''
+
+
+def island_perimeter(grid):
+    ''' compute perimiter of 1 spots in grid '''
+    peri = 0
+
+    length = len(grid)
+    width = len(grid[0])
+    if width == 0:
+        return peri
+    for ln in range(length):
+        for w in range(width - 1):
+            if grid[ln][w] != grid[ln][w + 1]:
+                peri += 1
+            if ln != length - 1 and grid[ln][w] != grid[ln + 1][w]:
+                peri += 1
+    return peri
