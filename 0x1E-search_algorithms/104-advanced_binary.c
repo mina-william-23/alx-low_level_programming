08dd5eae3d4a0ec063aa0680e196c9bd4ea8fcb0
mina-william-23 <capeandroid50@gmail.com>
2024-02-06 18:25:39 +0200

    advanced binary search

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -1,13 +1,13 @@
 #include "search_algos.h"
 /**
- * advanced_binary - searches for a value in an array of integers
- * using the Binary search algorithm
- * return the index of the first value in the array
- * (if this value appears more than once in the array)
- * @array: pointer to head of list
- * @size: size of array
- * @value: value to search
- * Return: found value or -1
+* advanced_binary - searches for a value in an array of integers
+* using the Binary search algorithm
+* return the index of the first value in the array
+* (if this value appears more than once in the array)
+* @array: pointer to head of list
+* @size: size of array
+* @value: value to search
+* Return: found value or -1
 */
 int advanced_binary(int *array, size_t size, int value)
 {
@@ -44,34 +44,43 @@ int advanced_binary(int *array, size_t size, int value)
 		ans = advanced_binary(array, i, value);
 	return (ans);
 }
-/* iterative way */
+/* iterative way*/
+
 /**
- * int advanced_binary(int *array, size_t size, int value)
- * {
- * 	size_t i, left, right, ans;
- * 
- * 	if (array == NULL)
- * 		return (-1);
- * 	left = 0;
- * 	right = size - 1;
- * 	ans = -1;
- * 	while (left < right)
- * 	{
- * 		printf("Searching in array: ");
- * 
- * 		for (i = left; i < right; i++)
- * 			printf("%d, ", array[i]);
- * 		
- * 		printf("%d\n", array[i]);
- * 
- * 		i = left + (right - left) / 2;
- * 		if (array[i] < value)
- * 			left = i + 1;
- * 		else if (array[i] >= value)
- * 			right = i;
- * 	}
- * 	if (array[right] == value)
- * 		ans = right;
- * 	return (ans);
- * }
+ * advanced_binary - searches for a value in an array of integers
+* using the Binary search algorithm
+* return the index of the first value in the array
+* (if this value appears more than once in the array)
+* @array: pointer to head of list
+* @size: size of array
+* @value: value to search
+* Return: found value or -1
+* int advanced_binary(int*array, size_t size, int value)
+* {
+* size_t i, left, right, ans;
+*
+* if (array == NULL)
+* return (-1);
+* left = 0;
+* right = size - 1;
+* ans = -1;
+* while (left < right)
+* {
+* printf("Searching in array: ");
+*
+* for (i = left; i < right; i++)
+* printf("%d, ", array[i]);
+*
+* printf("%d\n", array[i]);
+*
+* i = left + (right - left) / 2;
+* if (array[i] < value)
+* left = i + 1;
+* else if (array[i] >= value)
+* right = i;
+* }
+* if (array[right] == value)
+* ans = right;
+* return (ans);
+* }
 */
